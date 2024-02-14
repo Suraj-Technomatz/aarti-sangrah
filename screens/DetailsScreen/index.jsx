688eab3c289efc5378ae7e5738fc28c728ee4762
Suraj Verma <sverma@technomatz.com>
2024-02-14 16:00:03 +0530

    Make a list

--- screens/DetailsScreen/index.jsx
+++ screens/DetailsScreen/index.jsx
@@ -1,10 +1,64 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+import { godsName } from "../../utils/constant";
 
-export default function DetailsScreen() {
+export default function DetailsScreen({ route }) {
+  const { id } = route.params;
+
+  const getData = () => {
+    return godsName.filter((god) => god.id === id);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
+      <Text>{id}</Text>
+      {getData().map((god) => {
+        return (
+          <View key={god.id}>
+            <View>
+              <Image
+                style={styles.img}
+                source={{
+                  uri: god.imageUrl,
+                }}
+              />
+            </View>
+            <View style={styles.godHeadContainer}>
+              <Text style={styles.godHead}>{god.name}</Text>
+            </View>
+            {god.lyrics.map((lyric) => (
+              <View>
+                <Text style={styles.lyric}>{lyric}</Text>
+              </View>
+            ))}
+          </View>
+        );
+      })}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  godHeadContainer: {
+    marginVertical: 20,
+  },
+  godHead: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  lyricContainer: {},
+  lyric: {
+    color: "red",
+    fontWeight: "bold",
+    paddingVertical: 8,
+  },
+  imgContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  img: {
+    width: "auto",
+    height: 300,
+  },
+});
--- screens/HomeScreen/index.jsx
+++ screens/HomeScreen/index.jsx
@@ -1,57 +1,48 @@
 import React from "react";
-import { View, Text, Button, StyleSheet, Image } from "react-native";
-import { godsName } from "../../utils/constant";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { godsName } from "../../utils";
 
 export default function HomeScreen({ navigation }) {
+  const onHandlePress = (id) => {
+    navigation.navigate("Details", {
+      id,
+    });
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {godsName.map((god) => {
-        return (
-          <View>
+    <View style={{ flex: 1 }}>
+      {godsName.map((godN) => (
+        <View key={godN?.id} style={styles.containerForList}>
+          <TouchableOpacity onPress={() => onHandlePress(godN?.id)}>
             <View>
               <Image
                 style={styles.img}
                 source={{
-                  uri: god.imageUrl,
+                  uri: godN.imageUrl,
                 }}
               />
             </View>
-            <View style={styles.godHeadContainer}>
-              <Text style={styles.godHead}>{god.name}</Text>
-            </View>
-            {god.lyrics.map((lyric) => (
-              <View>
-                <Text style={styles.lyric}>{lyric}</Text>
-              </View>
-            ))}
-          </View>
-        );
-      })}
+            <Text style={{ fontWeight: "bold", fontSize: 18, marginLeft: 10 }}>
+              {godN.name}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  godHeadContainer: {
-    marginVertical: 20,
-  },
-  godHead: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  lyricContainer: {},
-  lyric: {
-    color: "red",
-    fontWeight: "bold",
-    paddingVertical: 8,
-  },
-  imgContainer: {
-    flex: 1,
-    justifyContent: "flex-start",
+  containerForList: {
+    flexDirection: "row",
     alignItems: "center",
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
   },
   img: {
-    width: "auto",
-    height: 300,
+    width: 50,
+    height: 50,
   },
 });
