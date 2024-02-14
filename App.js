37db08d56f6a7bfc1ddb80f6d0ea17bf8a67bfee
Suraj Verma <sverma@technomatz.com>
2024-02-14 13:14:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import "react-native-gesture-handler"; // Make sure to import this before using React Navigation
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
